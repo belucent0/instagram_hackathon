9068efe91fed47583c722cc0f9a86ec5bb1c90a6
vividnow <belucent0@gmail.com>
2023-04-29 14:47:19 +0900

    ããífix:feed image error

--- content/views.py
+++ content/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
 
-# Create your views here.
+
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all() # select * from content_feed;
+        for feed in feed_list:
+            print(feed.content)
+        return render(request, "kimstagram/main.html", context=dict(feeds=feed_list))
